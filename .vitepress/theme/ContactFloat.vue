ec28836ece48e4b1b0217b0a0468c6f9a691e596
mlogclub <mlog1@qq.com>
2025-04-17 22:11:05 +0800

    update

---
+++ .vitepress/theme/ContactFloat.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="contact-float">
+    <div class="contact-item">
+      <img src="/images/wechat-icon.svg" alt="微信" class="icon">
+      <span>微信</span>
+      <div class="qr-code">
+        <div class="qr-title">商务咨询</div>
+        <img src="/images/wechat_big.png" alt="微信二维码">
+      </div>
+    </div>
+    <div class="contact-item">
+      <img src="/images/qq-icon.svg" alt="QQ" class="icon">
+      <span>QQ群</span>
+      <div class="qr-code">
+        <div class="qr-title">BBS-GO交流群</div>
+        <img src="/images/qq.png" alt="QQ二维码">
+      </div>
+    </div>
+  </div>
+</template> 
--- .vitepress/theme/index.ts
+++ .vitepress/theme/index.ts
@@ -3,12 +3,14 @@ import { h } from 'vue'
 import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import './style.css'
+import ContactFloat from './ContactFloat.vue'
 
 export default {
   extends: DefaultTheme,
   Layout: () => {
     return h(DefaultTheme.Layout, null, {
       // https://vitepress.dev/guide/extending-default-theme#layout-slots
+      'layout-bottom': () => h(ContactFloat)
     })
   },
   enhanceApp({ app, router, siteData }) {
